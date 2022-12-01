806c15787da1753be6d1e39b208b221470f75c07
Pankaj Sharma <442001ps@gmail.com>
2022-12-01 18:12:16 +0530

    Franchise Form Update 01-12-22

--- assets/js/franchiseform.js
+++ assets/js/franchiseform.js
@@ -74,8 +74,8 @@ function secondNextHandler() {
   if (
     document.getElementById("name").value === "" ||
     document.getElementById("email").value === "" ||
-    document.getElementById("mobile-number").value === "" ||
-    document.getElementById("alt-mobile-number").value === ""
+    document.getElementById("mobile_number").value === "" ||
+    document.getElementById("alt_mobile_number").value === ""
   ) {
     document.getElementById("next2").disabled = true;
   } else {
@@ -85,8 +85,80 @@ function secondNextHandler() {
 
 // SUBMIT HANDLER ...
 
-document.getElementById("franchise-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  slidepage(12);
-  // slidepage(13);
+const scriptURL =
+  "https://academy-apiv3.herokuapp.com/api/V1/adddata/1hy5hwGEJ8gBvFYg-J2tfFIhux2B6ReFddgB-Ce1riKg";
+
+var forms = document.forms["franchise-form"];
+
+// FORM SUBMIT EVENT HANDLER
+
+forms.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  document.getElementById("submit-btn").innerText = "submitting ...";
+  console.log("Pankaj");
+
+  const formData = new FormData(forms);
+
+  // DDEFINE ALL THE FIELDS LIKE THIS IN VARIBLE
+
+  const investment = forms.elements["investment"];
+  const premises = forms.elements["premises"];
+  const address = forms.elements["address"];
+  const state = forms.elements["state"];
+  const city = forms.elements["city"];
+  const pincode = forms.elements["pincode"];
+  const experience = forms.elements["experience"];
+  const qualifications = forms.elements["qualifications"];
+  const area = forms.elements["area"];
+  const startIn = forms.elements["startIn"];
+  const staffReady = forms.elements["staffReady"];
+  const name = forms.elements["name"];
+  const email = forms.elements["email"];
+  const mobile_number = forms.elements["mobile_number"];
+  const alt_mobile_number = forms.elements["alt_mobile_number"];
+  const contactTime = forms.elements["contactTime"];
+  const Heard_From = forms.elements["Heard_From"];
+  const referral = forms.elements["referral"];
+
+  await fetch(scriptURL, {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify({
+      // DEFINE ALL THE FIELDS IN JSON FORMAT HERE
+      // ...formData,
+
+      investment: investment.value,
+      premises: premises.value,
+      address: address.value,
+      state: state.value,
+      city: city.value,
+      pincode: pincode.value,
+      experience: experience.value,
+      qualifications: qualifications.value,
+      area: area.value,
+      startIn: startIn.value,
+      staffReady: staffReady.value,
+      name: name.value,
+      email: email.value,
+      mobile_number: mobile_number.value,
+      alt_mobile_number: alt_mobile_number.value,
+      contactTime: contactTime.value,
+      Heard_From: Heard_From.value,
+      referral: referral.value,
+    }),
+  })
+    .then((response) => {
+      console.log(response);
+      slidepage(12);
+    })
+
+    .catch((err) => {
+      console.log(err);
+      slidepage(13);
+    });
 });
